30cbfddedc1af1878439d9a684e1d4c5456bdff2
Zack <sgps41106@gmail.com>
2024-01-01 19:52:21 +0800

    帳號功能 - 使用 MD5 加密密碼

--- spring-mall/src/main/java/com/zach/springmall/rowmapper/UserRowMapper.java
+++ spring-mall/src/main/java/com/zach/springmall/rowmapper/UserRowMapper.java
@@ -11,7 +11,7 @@ public class UserRowMapper implements RowMapper {
     public Object mapRow(ResultSet rs, int i) throws SQLException {
         User user = new User();
         user.setUserId(rs.getInt("user_id"));
-        user.setEmail(rs.getString("user_id"));
+        user.setEmail(rs.getString("email"));
         user.setPassword(rs.getString("password"));
         user.setCreatedDate(rs.getTimestamp("created_date"));
         user.setLastModifiedDate(rs.getTimestamp("last_modified_date"));
