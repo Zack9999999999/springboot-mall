840eb9a9d1859b9bdea908cbfdc8fab2d5130696
Zack <sgps41106@gmail.com>
2024-01-01 23:44:04 +0800

    訂單功能 - 創建訂單 (下)

--- spring-mall/src/main/java/com/zach/springmall/controller/OrderController.java
+++ spring-mall/src/main/java/com/zach/springmall/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.zach.springmall.controller;
 
 import com.zach.springmall.dto.CreateOrderRequest;
+import com.zach.springmall.model.Order;
 import com.zach.springmall.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,7 +24,9 @@ public class OrderController {
                                          @RequestBody @Valid CreateOrderRequest createOrderRequest){
         Integer orderId = orderService.createOrder(userId, createOrderRequest);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(orderId);
+        Order order = orderService.getOrderById(orderId);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(order);
     }
 
 }
--- spring-mall/src/main/java/com/zach/springmall/service/OrderService.java
+++ spring-mall/src/main/java/com/zach/springmall/service/OrderService.java
@@ -1,8 +1,10 @@
 package com.zach.springmall.service;
 
 import com.zach.springmall.dto.CreateOrderRequest;
+import com.zach.springmall.model.Order;
 
 public interface OrderService {
 
+    Order getOrderById(Integer orderId);
     Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest);
 }
